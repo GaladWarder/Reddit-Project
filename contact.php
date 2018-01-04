53654ec7359432bfc8ef95028162a759167669b3
GaladWarder <galaddota@yahoo.com>
2018-01-04 14:45:07 +0800

    more PHP tweaks
    trying to get it to send correctly

--- contact.php
+++ contact.php
@@ -1,40 +1,31 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'The Reddit Project'; 
-    $to = 'galadwarder@yahoo.com'; 
-    $subject = 'New Message!';
-    $human = $_POST['human'];
-			
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {  ?>			 
-        if (mail ($to, $subject, $body, $from)) { 
-	    <script language="javascript" type="text/javascript">
-			alert('Your message has been sent - I will be in contact with you soon!');
-			window.location = 'index.html';
-		</script>
-		
-	<?php
-	} 
-	else if ($_POST['submit'] && $human != '4') { ?>
-	
-		<script language="javascript" type="text/javascript">
-			alert('You answered the anti-spam question incorrectly...Are you sure you are human?');
-			window.location = 'index.html';
-		</script> 
-		
-	<?php
-	}
-	else { ?>
-	
-	    <script language="javascript" type="text/javascript">
-			alert('Oops! Something went wrong. Please email me directly at galaddota@yahoo.com');
-			window.location = 'index.html';
-		</script> 
-		
-	<?php
-    
-    }
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
+
+$mail_to = 'galaddota@yahoo.com';
+$subject = 'Reddit Project Message from'.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Your message has been sent - I will be in contact with you soon!');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please email me directly at galaddota@yahoo.com');
+		window.location = 'index.html';
+	</script>
+<?php
+}
 ?>
